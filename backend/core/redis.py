f6f7a8d77d2f353b496a39593033f111bac9053d
Chooook <egr311@ya.ru>
2025-05-18 23:03:21 +0400

    feat: add redis client module, update .end and settings

---
+++ backend/core/redis.py
@@ -0,0 +1,14 @@
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).parent.parent))
+
+import redis
+
+from settings import settings
+
+
+redis_client = redis.Redis(
+    host=settings.REDIS_HOST,
+    port=settings.REDIS_PORT,
+    db=settings.REDIS_DB)
--- settings.py
+++ settings.py
@@ -5,6 +5,9 @@ class Settings(BaseSettings):
     LOGLEVEL: str = 'INFO'
     DEBUG: bool = False
     DATABASE_URL: str
+    REDIS_HOST: str = 'localhost'
+    REDIS_PORT: int = 6379
+    REDIS_DB: int = 0
 
     class Config:
         env_file = '.env'
