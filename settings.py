1f87e0519172ac00843bbdb85c30758eb45fe69f
Chooook <egr311@ya.ru>
2025-05-18 23:57:47 +0400

    chore: remove unused DATABESE_URL

--- settings.py
+++ settings.py
@@ -4,7 +4,6 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     LOGLEVEL: str = 'INFO'
     DEBUG: bool = False
-    DATABASE_URL: str
     REDIS_HOST: str = 'localhost'
     REDIS_PORT: int = 6379
     REDIS_DB: int = 0
