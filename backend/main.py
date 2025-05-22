6b3ee3e316c8d692beff18c096143b77c1429604
Chooook <egr311@ya.ru>
2025-05-22 22:09:52 +0400

    feat: add fastapi cors middleware

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,27 @@
 import sys
 from pathlib import Path
 
-sys.path.append(str(Path(__file__).parent.parent))
-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+sys.path.append(str(Path(__file__).parent.parent))
 
 from settings import settings
 from api.v1.router import router as v1_router
 
+
 app = FastAPI(debug=settings.DEBUG)
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(v1_router)
