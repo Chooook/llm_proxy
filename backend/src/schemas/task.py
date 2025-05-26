4665f910ba1623c165cae62948606a220c59f6c2
Chooook <egr311@ya.ru>
2025-05-26 21:13:25 +0400

    feat: add task type to Task model (generate_gp)

--- backend/src/schemas/task.py
+++ backend/src/schemas/task.py
@@ -6,6 +6,7 @@ from pydantic import BaseModel
 class TaskType(str, Enum):
     GENERATE_WITH_API = 'generate_api'
     GENERATE_WITH_LOCAL = 'generate_local'
+    GENERATE_WITH_GP = 'generate_gp'
     SEARCH_IN_KNOWLEDGE_BASE = 'search_in_kb'
 
 class TaskCreate(BaseModel):
